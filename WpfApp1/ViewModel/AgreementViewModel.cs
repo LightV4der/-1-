6dd40cba1904420ddbb1f5f91d823c8e24a090cc
LightV4der <159343401+LightV4der@users.noreply.github.com>
2024-02-08 15:37:45 +0300

    Add files via upload

---
+++ WpfApp1/ViewModel/AgreementViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    class AgreementViewModel
+    {
+        public ObservableCollection<Agreement> ListAgreement { get; set; } = new ObservableCollection<Agreement>();
+        public AgreementViewModel()
+        {
+            this.ListAgreement.Add(
+            new Agreement
+            {
+                Id = 1,
+                Number = "1",
+                DataOpen = new DateTime(2012, 05, 20),
+                DataClose = new DateTime(2022, 05, 20)
+                
+            });
+            this.ListAgreement.Add(
+            new Agreement
+            {
+                Id = 2,
+                Number = "2",
+                DataOpen = new DateTime(2016, 07, 10),
+                DataClose = new DateTime(2026, 07, 10)
+            });
+            this.ListAgreement.Add(
+            new Agreement
+            {
+                Id = 3,
+                Number = "3",
+                DataOpen = new DateTime(2006, 06, 06),
+                DataClose = new DateTime(2066, 06, 06)
+            });
+        }
+        public int MaxId()
+        {
+            int max = 0;
+            foreach (var a in this.ListAgreement)
+            {
+                if (max < a.Id)
+                {
+                    max = a.Id;
+                };
+            }
+            return max;
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/BankViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    class BankViewModel
+    {
+        public int MaxId()
+        {
+            int max = 0;
+            foreach (var b in this.ListBank)
+            {
+                if (max < b.Id)
+                {
+                    max = b.Id;
+                };
+            }
+            return max;
+        }
+        public ObservableCollection<Bank> ListBank { get; set; } = new ObservableCollection<Bank>();
+        public BankViewModel()
+        {
+            this.ListBank.Add(
+            new Bank
+            {
+                Id = 1,
+                NameFull = "Публичное акционерное общество «Сбербанк России»\r\n",
+                NameShort = "Cбербанк",
+                Inn = "7707083893",
+                Bik = "044525225",
+                KorAccount = "30101810400000000225",
+                Account = "50234517800000002960",
+                City = "Moscow"
+            });
+            this.ListBank.Add(
+            new Bank
+            {
+                Id = 2,
+                NameFull = "Акционерное общество \"Тинькофф Банк\"\r\n",
+                NameShort = "АО \"Тинькофф Банк\"\r\n",
+                Inn = "7710140679\r\n",
+                Bik = "044525974",
+                KorAccount = "30101810145250000974",
+                Account = "50234517800000002960",
+                City = "Moscow"
+            });
+            this.ListBank.Add(
+            new Bank
+            {
+                Id = 3,
+                NameFull = "Акционерное общество \"Альфа Банк\"\r\n",
+                NameShort = "АО \"Альфа Банк\"\r\n",
+                Inn = "7728168971",
+                Bik = "044525593",
+                KorAccount = "30101810200000000593",
+                Account = "30101810200000000593",
+                City = "Moscow"
+            });
+        }
+    }
+}
